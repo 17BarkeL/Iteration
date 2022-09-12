f18022c4abef6a86bce0b39c4d00f96deb288e22
Luke Barkess <luke.barkess@icloud.com>
2022-09-12 14:55:13 +0100

    iterate

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,24 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+            // Repeat 561 times
+            /*for (int i = 1; i <= 561; i++)
+            {
+                Console.WriteLine("This is iteration number " + i);
+            }*/
+
+            // Repeat until user says "Hello"
+            string input = "";
+
+            while (input.ToUpper() != "HELLO")
+            {
+                Console.WriteLine("Hello?");
+                input = Console.ReadLine();
+            }
+
+
+
+            // Console.ReadLine();
         }
     }
 }
